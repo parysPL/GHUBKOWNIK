d743eef7940a5292d87989f708c7bd5ed5c33ecc
paicz <paicz@DESKTOP-UT2KT6A>
2025-05-29 20:08:49 +0200

    Dodaj pliki projektów.

---
+++ GHUBKOWNIK/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace GHUBKOWNIK
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
